c709515ee7bc40800b28cddf591cbfa51c5730d3
CarlosHerazo <Carlosherazo2004@gmail.com>
2024-06-19 18:53:32 -0500

    Cambio responsivo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const navbar = document.getElementById('header_nav');
 
-    // Agregar el evento de scroll
+    // add el event de scroll
     window.addEventListener('scroll', () => {
         // Si se ha desplazado más de 50 píxeles desde la parte superior
         if (window.scrollY > 50) {
@@ -12,3 +12,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+
+const btnBurger = document.querySelector('.fa-bars')
+
+btnBurger.addEventListener('click' , (e)=>{
+    document.querySelector('.nav_buger').style.display = 'block';
+})
